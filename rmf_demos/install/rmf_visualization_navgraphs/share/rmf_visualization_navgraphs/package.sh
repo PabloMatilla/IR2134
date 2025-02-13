89fb3bf0a0a732188d83342d95e946341e92850a
PabloMatilla <al415550@uji.es>
2025-02-13 18:41:57 +0100

    patrolling task complete

--- rmf_demos/install/rmf_visualization_navgraphs/share/rmf_visualization_navgraphs/package.sh
+++ rmf_demos/install/rmf_visualization_navgraphs/share/rmf_visualization_navgraphs/package.sh
@@ -80,6 +80,7 @@ _colcon_package_sh_source_script() {
 # source sh hooks
 _colcon_package_sh_source_script "$COLCON_CURRENT_PREFIX/share/rmf_visualization_navgraphs/hook/cmake_prefix_path.sh"
 _colcon_package_sh_source_script "$COLCON_CURRENT_PREFIX/share/rmf_visualization_navgraphs/hook/ld_library_path_lib.sh"
+_colcon_package_sh_source_script "$COLCON_CURRENT_PREFIX/share/rmf_visualization_navgraphs/local_setup.sh"
 
 unset _colcon_package_sh_source_script
 unset COLCON_CURRENT_PREFIX
